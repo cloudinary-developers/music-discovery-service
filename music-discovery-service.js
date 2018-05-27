08b81bab9b43f4e50cf6e345705aa3be95c9893a
Dan Zeitman <dvpweb@gmail.com>
2018-05-27 16:08:42 -0700

    Updating music-discovery-service.js

--- music-discovery-service.js
+++ music-discovery-service.js
@@ -598,6 +598,7 @@ app.get('/test/:releaseid', function ( req, res ) {
 
 var getArchivesByTag = function(tag) {  
   return new Promise(function (resolve, reject) {
+        cloudinary.config(clConfig);
         cloudinary.v2.api.resources(function(error, result){
             if(error){
                 reject(error);
