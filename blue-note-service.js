45ae2aff862207714ead1a9e5f27ea4b13af3930
Dan Zeitman <dvpweb@gmail.com>
2018-06-01 13:17:52 -0700

    Updating blue-note-service.js

--- blue-note-service.js
+++ blue-note-service.js
@@ -76,7 +76,7 @@ app.get('/boxscan/:page', function (req, res) {
   let cloudinary = context.cloudinary.secureAccess();
   let page = req.params.page || 1;
   try{
-  let image = cloudinary.image("assets/amy_winehouse/core/130751_LA_0011665_000_500_001", 
+  let image = cloudinary.image("assets/wayne_shorter/core/speak_no_evil/130751_LA_0011665_000_500_001", 
   {sign_url: true, type: "authenticated", 
   transformation: [
     {width: 800, page: page, crop: "scale"}
