d6c3871e3fd35b0b3cd5a52c2add210eca30422a
Dan Zeitman <dvpweb@gmail.com>
2018-06-01 14:20:16 -0700

    Updating blue-note-service.js

--- blue-note-service.js
+++ blue-note-service.js
@@ -92,8 +92,6 @@ app.get('/boxscan/:page', function (req, res) {
 app.get('/image/:transformation/*', function (req, res) {
   const context = req.webtaskContext;
   const transformation = req.params.transformation.split(',');
-  // console.log("params 0 = " + req.params[0]);
-  // console.log("params 1 = " + req.params[1]);
   const public_id = req.params[0];
   
   let transformations = [];
