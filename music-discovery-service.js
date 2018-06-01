1c7ef074e63284e6147d9230687c930c36f38b28
Dan Zeitman <dvpweb@gmail.com>
2018-05-31 17:15:19 -0700

    Updating music-discovery-service.js

--- music-discovery-service.js
+++ music-discovery-service.js
@@ -183,7 +183,7 @@ async function getMetaSeq(params){
     let lyricInfoOptions = {
             validateStatus: function (status) { return status < 500;},
                   params: {
-                    apikey: authenticated_secrets.lyricFind_api_key,
+                    apikey: authenticated_secrets.LyricFind_api_key,
                     territory:'US',
                     reqtype: 'default',
                     output: 'json',
